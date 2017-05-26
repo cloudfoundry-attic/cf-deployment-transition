f0d5ee274913d76821c1cfad2411692343031578
Joshua Aresty <joshua.aresty@emc.com>
2017-05-26 16:05:26 -0700

    [#145168515] Verify no cf-networking vars end up in varstore unintentionally
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- test/test-expected-varstore.sh
+++ test/test-expected-varstore.sh
@@ -22,6 +22,24 @@ DESCRIBE="When on the happy journey"
       fi
     popd > /dev/null
 
+  IT="when run without -N, makes a vars store from source manifests that does not include cf-networking vars"
+    pushd $(mktemp -d) > /dev/null
+      ${root_dir}/../transition.sh \
+        -cf ${root_dir}/fixture/source-cf-manifest.yml \
+        -d  ${root_dir}/fixture/source-diego-manifest-with-cf-networking.yml \
+        -ca ${root_dir}/fixture/ca-private-keys.yml > /dev/null
+
+      diff -wB -C5 ${root_dir}/fixture/expected-vars-store.yml deployment-vars.yml
+      status=$?
+
+      if [ "$status" == "0" ]; then
+        echo PASS - ${IT}
+      else
+        echo FAIL - ${IT}
+        examples_failed=1
+      fi
+    popd > /dev/null
+
   IT="makes a cf-networking vars store from source manifests"
     pushd $(mktemp -d) > /dev/null
       spiff merge ${root_dir}/fixture/expected-vars-store.yml \
