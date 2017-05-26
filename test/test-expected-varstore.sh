29e9f3dd44bd12cea560e2c990ca15304d8f46e4
Joshua Aresty <joshua.aresty@emc.com>
2017-05-26 15:33:30 -0700

    [#145168515] Test -N flag extracts cf-networking properties into varstore
    Signed-off-by: Steve Taylor <staylor@pivotal.io>

--- test/test-expected-varstore.sh
+++ test/test-expected-varstore.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 root_dir=$PWD/$(dirname $0)
+examples_failed=0
+
 DESCRIBE="When on the happy journey"
   IT="makes a vars store from source manifests"
     pushd $(mktemp -d) > /dev/null
       ${root_dir}/../transition.sh \
         -cf ${root_dir}/fixture/source-cf-manifest.yml \
-        -d ${root_dir}/fixture/source-diego-manifest.yml \
+        -d  ${root_dir}/fixture/source-diego-manifest.yml \
         -ca ${root_dir}/fixture/ca-private-keys.yml > /dev/null
 
       diff -wB -C5 ${root_dir}/fixture/expected-vars-store.yml deployment-vars.yml
@@ -16,7 +18,37 @@ DESCRIBE="When on the happy journey"
         echo PASS - ${IT}
       else
         echo FAIL - ${IT}
+        examples_failed=1
       fi
     popd > /dev/null
 
-exit $status
+  IT="makes a cf-networking vars store from source manifests"
+    pushd $(mktemp -d) > /dev/null
+      spiff merge ${root_dir}/fixture/expected-vars-store.yml \
+        ${root_dir}/fixture/expected-cf-networking-vars.yml > expected-vars-store.yml
+
+      ${root_dir}/../transition.sh \
+        -cf ${root_dir}/fixture/source-cf-manifest.yml \
+        -d  ${root_dir}/fixture/source-diego-manifest-with-cf-networking.yml \
+        -ca ${root_dir}/fixture/source-ca-private-keys-cf-networking.yml \
+        -N > /dev/null
+
+      diff -wB -C5 expected-vars-store.yml deployment-vars.yml
+      status=$?
+
+      if [ "$status" == "0" ]; then
+        echo PASS - ${IT}
+      else
+        echo FAIL - ${IT}
+        examples_failed=1
+      fi
+    popd > /dev/null
+
+if [[ "${examples_failed}" > 0 ]]; then
+  echo ${DESCRIBE} FAILED!
+  exit 1
+else
+  echo ${DESCRIBE} PASSED!
+  exit 0
+fi
+
